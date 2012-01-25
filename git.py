991b819e9dbf8de14a4bdc712d720b4fe82b0dc7
Sheldon <sheldon.els@gmail.com>
2012-01-25 19:44:02 +0000

    was throwing an error sometimes when passing along stdin as a named param

--- git.py
+++ git.py
@@ -178,7 +178,7 @@ class GitCommand(object):
         self.panel(result)
 
     def _output_to_view(self, output_file, output, clear=False,
-            syntax="Packages/Diff/Diff.tmLanguage"):
+            syntax="Packages/Diff/Diff.tmLanguage", **kwargs):
         output_file.set_syntax_file(syntax)
         edit = output_file.begin_edit()
         if clear:
