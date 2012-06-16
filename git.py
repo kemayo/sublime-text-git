af174d9eb978ec7fb05c47bb5d46785ae0958235
German M. Bravo <german.mb@deipi.com>
2012-06-16 12:00:40 -0500

    Added diffs support for renamed files and with older diffs

--- git.py
+++ git.py
@@ -430,10 +430,46 @@ class GitGraphAllCommand(GitGraph, GitWindowCommand):
 
 class GitDiff (object):
     def run(self, edit=None):
-        self.run_command(['git', 'diff', '--no-color', '--', self.get_file_name()],
-            self.diff_done)
+        self.run_command(
+            ['git', 'log', '--follow', '--name-only', '--pretty=%s\a%h %an <%aE>\a%ad (%ar)',
+            '--date=local', '--max-count=9000', '--', self.get_file_name()],
+            self.show_done)
 
-    def diff_done(self, result):
+    def show_done(self, result):
+        # GitLog Copy-Past
+        import os
+        self.results = []
+        self.files = {}
+        relative = None
+        for r in result.strip().split('\n'):
+            if r:
+                _result = r.strip().split('\a', 2)
+                if len(_result) == 1:
+                    if relative is None:
+                        # Find relative path
+                        relative = os.sep.join(['..'] * (len(os.path.normpath(_result[0]).split(os.sep)) - 1))
+                        if relative:
+                            relative += os.sep
+                    self.files[result[1].split(' ')[0]] = relative + _result[0]
+                else:
+                    result = _result
+                    self.results.append(result)
+        self.quick_panel(self.results, self.panel_done)
+
+    def panel_done(self, picked):
+        if 0 > picked < len(self.results):
+            return
+        item = self.results[picked]
+        # the commit hash is the first thing on the second line
+        ref = item[1].split(' ')[0]
+        command = ['git', 'diff', '-C', '--no-color', ref, '--']
+        command.extend(set(self.files.values() + [self.get_file_name()]))
+        self.run_command(
+            command,
+            self.diff_done,
+            ref=ref)
+
+    def diff_done(self, result, ref):
         if not result.strip():
             self.panel("No output")
             return
@@ -452,14 +488,24 @@ class GitDiff (object):
 
 class GitDiffCommit (object):
     def run(self, edit=None):
-        self.run_command(['git', 'diff', '--cached', '--no-color'],
+        self.run_command(['git', 'diff', '-C', '--no-color', '--cached'],
             self.diff_done)
 
     def diff_done(self, result):
         if not result.strip():
             self.panel("No output")
             return
-        self.scratch(result, title="Git Diff")
+        s = sublime.load_settings("Git.sublime-settings")
+        if s.get('diff_panel'):
+            view = self.panel(result)
+        else:
+            view = self.scratch(result, title="Git Diff")
+
+        lines_inserted = view.find_all(r'^\+[^+]{2} ')
+        lines_deleted = view.find_all(r'^-[^-]{2} ')
+
+        view.add_regions("inserted", lines_inserted, "markup.inserted.diff", "dot", sublime.HIDDEN)
+        view.add_regions("deleted", lines_deleted, "markup.deleted.diff", "dot", sublime.HIDDEN)
 
 
 class GitDiffCommand(GitDiff, GitTextCommand):
