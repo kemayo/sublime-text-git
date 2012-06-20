3391c70cdaaabdf876192bfbcb7fc179862ff069
Sheldon Els <sheldon.els@gmail.com>
2012-06-20 15:35:50 +0100

    check path correctly, needed root

--- git.py
+++ git.py
@@ -672,7 +672,7 @@ class GitStatusCommand(GitWindowCommand):
         s = sublime.load_settings("Git.sublime-settings")
         root = git_root(self.get_working_dir())
         if picked_status == '??' or s.get('status_opens_file') or self.force_open:
-            if(os.path.isfile(picked_file)): self.window.open_file(os.path.join(root, picked_file))
+            if(os.path.isfile(os.path.join(root, picked_file))): self.window.open_file(os.path.join(root, picked_file))
         else:
             self.run_command(['git', 'diff', '--no-color', '--', picked_file.strip('"')],
                 self.diff_done, working_dir=root)
