5e9696c8e37cc5738fc7ebd30a1c9b704bfdbb35
David Lynch <kemayo@gmail.com>
2012-09-14 13:41:31 -0500

    Whether to show the branch in status: configurable

--- statusbar.py
+++ statusbar.py
@@ -1,24 +1,20 @@
+import sublime
 import sublime_plugin
 from git import GitTextCommand
 
 
 class GitBranchStatusListener(sublime_plugin.EventListener):
-    def on_load(self, view):
+    def on_activated(self, view):
         view.run_command("git_branch_status")
 
 
 class GitBranchStatusCommand(GitTextCommand):
-    branch = ""
-
     def run(self, view):
-        if self.__class__.branch:
-            self.set_status(self.__class__.branch)
-        else:
+        s = sublime.load_settings("Git.sublime-settings")
+        if s.get("statusbar_branch"):
             self.run_command(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], self.branch_done, show_status=False)
+        else:
+            self.view.set_status("git-branch", "")
 
     def branch_done(self, result):
-        self.__class__.branch = result.strip()
-        self.set_status(self.__class__.branch)
-
-    def set_status(self, branch):
-        self.view.set_status("git-branch", "git branch: " + branch)
+        self.view.set_status("git-branch", "git branch: " + result.strip())
