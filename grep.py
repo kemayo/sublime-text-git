ab226a069284f3ce6d9bb8ae930f84ab7360a8fc
Rubycut <ruby.cutter@gmail.com>
2012-12-20 10:42:43 +0100

    support result filtering

--- grep.py
+++ grep.py
@@ -8,33 +8,35 @@ class GitGrepCommand(GitWindowCommand):
   # init, setup some default object properties
   def __init__(self, window):
     self.last_query = ''
+    self.last_ignore = ''
     # http://docs.python.org/library/functions.html#hasattr
     if hasattr(sublime_plugin.WindowCommand, '__init__'):
             sublime_plugin.WindowCommand.__init__(self, window)
 
   # this is first executed with window.run_command("git_grep") or key shortcut
-  def run(self):    
-    self.window.show_input_panel('git grep', self.last_query, self.on_done, self.on_change, self.on_cancel)
-
-  def on_done(self, query):
-
+  def run(self):
+    self.window.show_input_panel('git grep', self.last_query, self.on_grep_done, self.on_change, self.on_cancel)
+  def on_grep_done(self,query):
     self.last_query = query
+    self.window.show_input_panel('ignore files(use | to specify more)', self.last_ignore, self.on_done, self.on_change, self.on_cancel)
 
+  def on_done(self, ignore):
+    self.last_ignore = ignore
     # -i ignore case, -n return numbers, -I skip binary files
-    
-    status, out = commands.getstatusoutput('git grep --full-name -Iin "%s" "%s"' % (query, git_root(self.get_working_dir())) )    
+
+    status, out = commands.getstatusoutput('git grep --full-name -Iin "%s" "%s"|grep -Ev "%s"' % (self.last_query, git_root(self.get_working_dir()), ignore))
 
     # decod utf 8 and split to line array
     self.out_list = out.decode('ascii', 'ignore').split("\n")
     print self.out_list
-    if(self.out_list==[""]):      
+    if(self.out_list==[""]):
       sublime.message_dialog("""Can't find "%s" \n in git repo based in: %s\n """ % (query, git_root(self.get_working_dir()) ))
       return
 
     def split(l):
         # split line on 3 parts, what if I have : in actual line?
         # problem here, if nothing returned from git grep, I can't split empty line on 3 parts
-        # Return a list of the words in the string, using sep as the delimiter string. 
+        # Return a list of the words in the string, using sep as the delimiter string.
         # If maxsplit is given, at most maxsplit splits are done (thus, the list will have at most maxsplit+1 elements).
         fname, line, match = l.split(":", 2)
         return [match.strip()[0:100], ":".join([fname, line])]
@@ -47,13 +49,13 @@ class GitGrepCommand(GitWindowCommand):
   def on_change(self, arg):
         return
   def on_cancel(self):
-        return        
+        return
   def on_done_sel(self,index):
       if index == -1:
           return
       # out list we already parsed in on_done
       line = self.out_list[index]
-      filename, lineno, match = line.split(":", 2)      
-      
-      path = git_root(self.get_working_dir())              
+      filename, lineno, match = line.split(":", 2)
+
+      path = git_root(self.get_working_dir())
       self.window.open_file(os.path.join(path, filename) + ':' + lineno, sublime.ENCODED_POSITION)
