40d85ab3964f5279c135bca290b44d3320c9c55b
David Capello <davidcapello@gmail.com>
2012-12-29 02:52:51 -0300

    Add git_goto_diff command to jump to the diff line with Enter key

--- diff.py
+++ diff.py
@@ -1,5 +1,30 @@
 import sublime
+import re
 from git import GitTextCommand, GitWindowCommand
+import functools
+
+# Stores the path related to a "Git Diff" view.
+git_diff_path = {}
+
+
+def do_when(conditional, callback, *args, **kwargs):
+    if conditional():
+        return callback(*args, **kwargs)
+    sublime.set_timeout(functools.partial(do_when, conditional, callback, *args, **kwargs), 50)
+
+
+def goto_xy(view, line, col):
+    view.run_command("goto_line", {"line": line})
+    for i in range(col):
+        view.run_command("move", {"by": "characters", "forward": True})
+
+
+class GitDiffListener(sublime_plugin.EventListener):
+    def on_close(self, view):
+        global git_diff_path
+        if view.name() != "Git Diff":
+            return
+        git_diff_path.pop(view.id())
 
 
 class GitDiff (object):
@@ -8,6 +33,8 @@ class GitDiff (object):
                          self.diff_done)
 
     def diff_done(self, result):
+        global git_diff_path
+
         if not result.strip():
             self.panel("No output")
             return
@@ -22,6 +49,8 @@ class GitDiff (object):
 
         view.add_regions("inserted", lines_inserted, "markup.inserted.diff", "dot", sublime.HIDDEN)
         view.add_regions("deleted", lines_deleted, "markup.deleted.diff", "dot", sublime.HIDDEN)
+
+        git_diff_path[view.id()] = self.get_working_dir()
 
 
 class GitDiffCommit (object):
@@ -60,3 +89,46 @@ class GitDiffToolCommand(GitDiffTool, GitTextCommand):
 class GitDiffToolAll(GitWindowCommand):
     def run(self):
         self.run_command(['git', 'difftool'])
+
+
+class GitGotoDiff(sublime_plugin.TextCommand):
+    def run(self, edit):
+        v = self.view
+        if v.name() != "Git Diff":
+            return
+
+        beg = v.sel()[0].a     # Current position in selection
+        pt = v.line(beg).a     # First position in the current diff line
+        column = beg - pt - 1  # The current column (-1 because the first char in diff file)
+
+        file_name = None
+        hunk_line = None
+        line_offset = 0
+
+        while pt > 0:
+            line = v.line(pt)
+            lineContent = v.substr(line)
+            if lineContent.startswith("@@"):
+                if not hunk_line:
+                    hunk_line = lineContent
+            elif lineContent.startswith("+++ b/"):
+                file_name = v.substr(sublime.Region(line.a+6, line.b))
+                break
+            elif not hunk_line and not lineContent.startswith("-"):
+                line_offset = line_offset+1
+
+            pt = v.line(pt-1).a
+
+        hunk = re.match(r"^@@ -(\d+),(\d+) \+(\d+),(\d+) @@.*", hunk_line)
+        if not hunk:
+            sublime.status_message("No hunk info")
+            return
+
+        hunk_start_line = hunk.group(3)
+        goto_line = int(hunk_start_line) + line_offset - 1
+
+        file_name = os.path.join(git_diff_path[v.id()], file_name)
+
+        new_view = self.view.window().open_file(file_name)
+        do_when(lambda: not new_view.is_loading(),
+                lambda: goto_xy(new_view, goto_line, column))
