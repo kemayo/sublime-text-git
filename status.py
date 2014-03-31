970fd2172d3fbe9cdac90cb844e47aa19f5f5518
Starli0n <Starli0n@users.noreply.github.com>
2014-03-31 11:51:09 +0200

    Fix Issue #336

--- status.py
+++ status.py
@@ -43,8 +43,9 @@ class GitStatusCommand(GitWindowCommand):
         s = sublime.load_settings("Git.sublime-settings")
         root = git_root(self.get_working_dir())
         if picked_status == '??' or s.get('status_opens_file') or self.force_open:
-            if(os.path.isfile(os.path.join(root, picked_file))):
-                self.window.open_file(os.path.join(root, picked_file))
+            file_name = os.path.join(os.path.realpath(root), picked_file)
+            if os.path.isfile(file_name):
+                self.window.open_file(file_name)
         else:
             self.run_command(['git', 'diff', '--no-color', '--', picked_file.strip('"')],
                 self.diff_done, working_dir=root)
