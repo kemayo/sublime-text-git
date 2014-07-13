34d32b960a780923e2685e4bd46c536d66b77a54
David Lynch <kemayo@gmail.com>
2014-07-13 17:18:27 -0500

    Don't gutter the diff icons any more. Fixes #356.

--- diff.py
+++ diff.py
@@ -23,12 +23,6 @@ class GitDiff (object):
         else:
             view = self.scratch(result, title="Git Diff", syntax=syntax)
 
-        lines_inserted = view.find_all(r'^\+[^+]{2} ')
-        lines_deleted = view.find_all(r'^-[^-]{2} ')
-
-        view.add_regions("inserted", lines_inserted, "markup.inserted.diff", "dot", sublime.HIDDEN)
-        view.add_regions("deleted", lines_deleted, "markup.deleted.diff", "dot", sublime.HIDDEN)
-
         # Store the git root directory in the view so we can resolve relative paths
         # when the user wants to navigate to the source file.
         view.settings().set("git_root_dir", git_root(self.get_working_dir()))
